caf452be2ed2b359aaa8b71e39f9c9d76c103e2d
Will Liu <willliu2001@gmail.com>
2025-02-08 15:31:30 -0500

    Hello World added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
+=======
+import './App.css';
+import Home from './components/Home';
+>>>>>>> a97520a (Hello World added)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <p>Hello World</p>
+=======
+      <Home />
+>>>>>>> a97520a (Hello World added)
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Hellow worlds
+    </div>
+  )
+}
+
+export default Home
